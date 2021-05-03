60311a6124a0e8165794de5b51e67ff476646fb7
Ryan Schostag <ryanschostag@gmail.com>
2021-05-03 14:04:21 -0600

    Feat: added a few more tess

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,6 +8,7 @@ Requirements:
 uvicorn must be running the app:my_app FastAPI instance 
 This works with the chinook database. 
 """
+import ast
 import json
 import requests
 import pytest 
@@ -25,7 +26,24 @@ def test_app_root_keys():
     # Test that "/" returns usage information 
     session = requests.Session()
     response = session.get('http://127.0.0.1:8000/') 
+    session.close()
     response_dict = json.loads(response.text)
     assert sorted(response_dict.keys()) == ['apis', 'message']
     assert sorted(response_dict['apis'].keys()) == ['query', 'tables', 'tables/info']
+
+
+def test_app_table_names():
+    # Tests that the tables URI works 
+    session = requests.Session()
+    response = session.get('http://127.0.0.1:8000/tables/all')
+    session.close()
+    assert isinstance(ast.literal_eval(response.text), list)
+
+
+# @pytest.xfail('This is a test for the chinook database only')
+def test_app_table_info():
+    # Tests that the tables/info URI works 
+    session = requests.Session()
+    response = session.get('http://127.0.0.1:8000/tables/info/albums')
+    assert response.text
 
