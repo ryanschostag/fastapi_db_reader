3fc7d44fd5b3ec564f8c6af8cd6893ec4dcad5b9
Ryan Schostag <ryanschostag@gmail.com>
2021-05-03 19:31:59 -0600

    Fix: cleaned up and changed log path

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 db_path = './db/chinook.db'
 db_name = 'chinook'
-prohibited_query_keywords = ['ALTER', 'ATTACH', 'COMMIT', 'CREATE', 'DATABASE', 'DELETE', 'DETACH', 'DROP', 'INDEX', 'INSERT', 'PRAGMA', 'TABLE', 'TEMP', 'TEMPORARY', 'TRANSACTION', 'TRIGGER', 'UPDATE', 'VACUUM']
+prohibited_query_keywords = ['ALTER', 'ATTACH', 'COMMIT', 'CREATE', 'DATABASE', 
+                             'DELETE', 'DETACH', 'DROP', 'INDEX', 'INSERT', 
+                             'PRAGMA', 'TABLE', 'TEMP', 'TEMPORARY', 'TRANSACTION', 
+                             'TRIGGER', 'UPDATE', 'VACUUM']
 
+# logging configuration
+log_name = 'fastapi_app'
+log_file_path = '/var/log/fastapi_app.log'  # to avoid circular reference, do not use a file in the app folder
+log_format = '%[(time)s %(process)s %(file)s %(line)s %(func)s] %(message)s'
