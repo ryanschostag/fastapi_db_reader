a963ca1524a52f8b1e6dcc75c863c37961ba4fee
Ryan Schostag <ryanschostag@gmail.com>
2021-05-03 18:26:05 -0600

    Feat: added tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -32,18 +32,40 @@ def test_app_root_keys():
     assert sorted(response_dict['apis'].keys()) == ['query', 'tables', 'tables/info']
 
 
-def test_app_table_names():
-    # Tests that the tables URI works 
+@pytest.mark.parametrize('command,expected_key', [
+    ('all', 'table_names'),
+    ('fake', 'error')
+])
+def test_app_get_tables(command, expected_key):
+    # Tests that the tables URI works; It should return a dictionary
+    # if all is not the command passed, it should return an error 
     session = requests.Session()
-    response = session.get('http://127.0.0.1:8000/tables/all')
+    response = session.get(f'http://127.0.0.1:8000/tables/{command}')
+    response_dict = json.loads(response.text)
     session.close()
-    assert isinstance(ast.literal_eval(response.text), list)
+    assert 'error' in response_dict.keys() if expected_key == 'error' else 'table_names' in response_dict.keys()
 
 
-# @pytest.xfail('This is a test for the chinook database only')
+@pytest.mark.xfail(msg='This is a test for the chinook database only')
 def test_app_table_info():
     # Tests that the tables/info URI works 
     session = requests.Session()
     response = session.get('http://127.0.0.1:8000/tables/info/albums')
-    assert response.text
+    assert json.loads(response.text)
 
+
+@pytest.mark.xfail(msg='This is a test for the chinook database only')
+@pytest.mark.parametrize('sql_query,expected_result', [
+    ('SELECT * FROM employees', 'non-empty'),
+    ('DROP employees', [])
+])
+def test_app_query(sql_query, expected_result):
+    # Tests that the API works as expected: return [] if prohibited keywords are used else a non-empty list 
+    # Tests that the result is JSON serializable 
+    session = requests.Session()
+    received_dict = json.loads(session.get(f'http://127.0.0.1:8000/query/{sql_query}').text)
+    session.close()
+    assert received_dict if expected_result == 'non-empty' else not received_dict
+
+
+    
